639d7f93291699a6563114e5a1f501c6b8bb5b3c
masx200 <masx200@qq.com>
2025-08-12 04:16:39 +0100

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -11,7 +11,7 @@ const DB_PATH = path.join(__dirname, 'geminicliui_auth.db');
 const INIT_SQL_PATH = path.join(__dirname, 'init.sql');
 
 // Create database connection
-const db = new Database(DB_PATH);
+const db = new Database(process.env.DB_PATH || DB_PATH);
 // console.log('Connected to SQLite database');
 
 // Initialize database with schema
